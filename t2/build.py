88522ea89160b67876c6530cff1b23be7de2cdf7
caihedong <caihedongorder@126.com>
2017-12-25 17:11:25 +0800

    =add t2

---
+++ t2/build.py
@@ -0,0 +1,36 @@
+
+import os.path as path
+import os
+import argparse
+
+curdir = path.abspath(os.curdir)
+cmakefile_dir = path.join(curdir,"CMakeFiles")
+build_dir = path.join(curdir,"cmake_build")
+
+
+def remove_project_file():
+    os.system("rm -rf " + build_dir)
+    os.system("rm -rf " + cmakefile_dir)
+    print("clean finish!")
+
+def cmake_build_project():
+    print("Begin Building...")
+    print("current Path:"+ curdir)
+    print("cmake build dir:"+ build_dir)
+
+    if not path.exists(build_dir):
+        os.system("mkdir " + build_dir)
+
+    os.chdir(build_dir)
+
+    os.system("cmake ..")
+
+parser = argparse.ArgumentParser(description="build help")
+parser.add_argument('--clean',action = "store_true",help="clean project")
+args = parser.parse_args()
+
+print("args:"+str(vars(args)))
+if args.clean:
+    remove_project_file()
+else:
+    cmake_build_project()
---
+++ t2/src/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello,World From t1 Main");
+    return 0;
+}
